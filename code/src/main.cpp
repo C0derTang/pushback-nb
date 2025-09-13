686fd3825735b2690f1376baa0cd557187b83d99
C0dertang <tangchristopher111@gmail.com>
2025-09-13 13:29:30 -0700

    motor setup and dt code

--- code/src/main.cpp
+++ code/src/main.cpp
@@ -1,93 +1,147 @@
-/*----------------------------------------------------------------------------*/
-/*                                                                            */
-/*    Module:       main.cpp                                                  */
-/*    Author:       tangc                                                     */
-/*    Created:      9/13/2025, 11:58:27 AM                                    */
-/*    Description:  V5 project                                                */
-/*                                                                            */
-/*----------------------------------------------------------------------------*/
+#include "main.h"
+#include "lemlib/api.hpp"
 
-#include "vex.h"
+pros::Controller sticks(pros::E_CONTROLLER_MASTER);
 
-using namespace vex;
+/*------------------------- drivetrain + motors ---------------------------------*/
+pros::MotorGroup lDrive({-8, -9, -10}, pros::MotorGearset::blue);
+pros::MotorGroup rDrive({1, 2, 3}, pros::MotorGearset::blue);
 
-// A global instance of competition
-competition Competition;
+lemlib::Drivetrain drivetrain(&lDrive, // left motor group
+                              &rDrive, // right motor group
+                              9.567, // inch track width
+                              lemlib::Omniwheel::NEW_325, // wheel type
+                              450, // drivetrain rpm is 360
+                              2 // horizontal drift is 2 (for now)
+);
 
-// define your global instances of motors and other devices here
+/*------------------------ odom + PID config ------------------------------------*/
 
-/*---------------------------------------------------------------------------*/
-/*                          Pre-Autonomous Functions                         */
-/*                                                                           */
-/*  You may want to perform some actions before the competition starts.      */
-/*  Do them in the following function.  You must return from this function   */
-/*  or the autonomous and usercontrol tasks will not be started.  This       */
-/*  function is only called once after the V5 has been powered on and        */
-/*  not every time that the robot is disabled.                               */
-/*---------------------------------------------------------------------------*/
+/* example things
+pros::Rotation horizontal_encoder(20);
+pros::adi::Encoder vertical_encoder('C', 'D', true);
+lemlib::TrackingWheel horizontal_tracking_wheel(&horizontal_encoder, lemlib::Omniwheel::NEW_275, -5.75);
+lemlib::TrackingWheel vertical_tracking_wheel(&vertical_encoder, lemlib::Omniwheel::NEW_275, -2.5);
+*/
 
-void pre_auton(void) {
+lemlib::OdomSensors sensors(nullptr, // vertical tracking wheel 1, set to null
+                            nullptr, // vertical tracking wheel 2, set to nullptr as we are using IMEs
+                            nullptr, // horizontal tracking wheel 1
+                            nullptr, // horizontal tracking wheel 2, set to nullptr as we don't have a second one
+                            nullptr // inertial sensor
+);
 
-  // All activities that occur before the competition starts
-  // Example: clearing encoders, setting servo positions, ...
+lemlib::ControllerSettings lateralPID(10, // proportional gain (kP)
+                                    	0, // integral gain (kI)
+                                        3, // derivative gain (kD)
+                                        3, // anti windup
+                                        1, // small error range, in inches
+                                        100, // small error range timeout, in milliseconds
+                                        3, // large error range, in inches
+                                        500, // large error range timeout, in milliseconds
+                                        20 // maximum acceleration (slew)
+);
+
+// angular PID controller
+lemlib::ControllerSettings angularPID(2, // proportional gain (kP)
+                                        0, // integral gain (kI)
+                                        10, // derivative gain (kD)
+                                        3, // anti windup
+                                        1, // small error range, in degrees
+                                        100, // small error range timeout, in milliseconds
+                                        3, // large error range, in degrees
+                                        500, // large error range timeout, in milliseconds
+                                        0 // maximum acceleration (slew)
+);
+
+
+lemlib::ExpoDriveCurve throttle(3, // joystick deadband out of 127
+                                10, // minimum output where drivetrain will move out of 127
+                                1.019 // expo curve gain
+);
+
+lemlib::ExpoDriveCurve steer(3, // joystick deadband out of 127
+                             10, // minimum output where drivetrain will move out of 127
+                             1.019 // expo curve gain
+);
+
+
+// create the chassis
+lemlib::Chassis chassis(drivetrain, // drivetrain settings
+                        lateralPID, // lateral PID settings
+                        angularPID, // angular PID settings
+                        sensors, // odometry sensors
+						&throttle, 
+                        &steer
+);
+
+
+
+
+/**
+ * Runs initialization code. This occurs as soon as the program is started.
+ *
+ * All other competition modes are blocked by initialize; it is recommended
+ * to keep execution time for this mode under a few seconds.
+ */
+void initialize() {
+	pros::lcd::initialize();
+	pros::lcd::set_text(1, "Hello PROS User!");
 }
 
-/*---------------------------------------------------------------------------*/
-/*                                                                           */
-/*                              Autonomous Task                              */
-/*                                                                           */
-/*  This task is used to control your robot during the autonomous phase of   */
-/*  a VEX Competition.                                                       */
-/*                                                                           */
-/*  You must modify the code to add your own robot specific commands here.   */
-/*---------------------------------------------------------------------------*/
+/**
+ * Runs while the robot is in the disabled state of Field Management System or
+ * the VEX Competition Switch, following either autonomous or opcontrol. When
+ * the robot is enabled, this task will exit.
+ */
+void disabled() {}
 
-void autonomous(void) {
-  // ..........................................................................
-  // Insert autonomous user code here.
-  // ..........................................................................
-}
+/**
+ * Runs after initialize(), and before autonomous when connected to the Field
+ * Management System or the VEX Competition Switch. This is intended for
+ * competition-specific initialization routines, such as an autonomous selector
+ * on the LCD.
+ *
+ * This task will exit when the robot is enabled and autonomous or opcontrol
+ * starts.
+ */
+void competition_initialize() {}
 
-/*---------------------------------------------------------------------------*/
-/*                                                                           */
-/*                              User Control Task                            */
-/*                                                                           */
-/*  This task is used to control your robot during the user control phase of */
-/*  a VEX Competition.                                                       */
-/*                                                                           */
-/*  You must modify the code to add your own robot specific commands here.   */
-/*---------------------------------------------------------------------------*/
+/**
+ * Runs the user autonomous code. This function will be started in its own task
+ * with the default priority and stack size whenever the robot is enabled via
+ * the Field Management System or the VEX Competition Switch in the autonomous
+ * mode. Alternatively, this function may be called in initialize or opcontrol
+ * for non-competition testing purposes.
+ *
+ * If the robot is disabled or communications is lost, the autonomous task
+ * will be stopped. Re-enabling the robot will restart the task, not re-start it
+ * from where it left off.
+ */
+void autonomous() {}
 
-void usercontrol(void) {
-  // User control code here, inside the loop
-  while (1) {
-    // This is the main execution loop for the user control program.
-    // Each time through the loop your program should update motor + servo
-    // values based on feedback from the joysticks.
+/**
+ * Runs the operator control code. This function will be started in its own task
+ * with the default priority and stack size whenever the robot is enabled via
+ * the Field Management System or the VEX Competition Switch in the operator
+ * control mode.
+ *
+ * If no competition control is connected, this function will run immediately
+ * following initialize().
+ *
+ * If the robot is disabled or communications is lost, the
+ * operator control task will be stopped. Re-enabling the robot will restart the
+ * task, not resume it from where it left off.
+ */
+void opcontrol() {
+    while (true){
+        int leftY = sticks.get_analog(pros::E_CONTROLLER_ANALOG_LEFT_Y);
+        int rightX = sticks.get_analog(pros::E_CONTROLLER_ANALOG_RIGHT_X);
 
-    // ........................................................................
-    // Insert user code here. This is where you use the joystick values to
-    // update your motors, etc.
-    // ........................................................................
+        // move the robot
+        chassis.curvature(leftY, rightX);
 
-    wait(20, msec); // Sleep the task for a short amount of time to
-                    // prevent wasted resources.
-  }
-}
-
-//
-// Main will set up the competition functions and callbacks.
-//
-int main() {
-  // Set up callbacks for autonomous and driver control periods.
-  Competition.autonomous(autonomous);
-  Competition.drivercontrol(usercontrol);
-
-  // Run the pre-autonomous function.
-  pre_auton();
-
-  // Prevent main from exiting with an infinite loop.
-  while (true) {
-    wait(100, msec);
-  }
+        // delay to save resources
+        pros::delay(25);
+    }
 }
